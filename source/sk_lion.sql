2dc5fad998d4907bb2c4c91a3a457fb96c0cadfa
havizIM <viz.ndinq@gmail.com>
2019-08-19 13:48:57 +0100

    Update Haviz

--- source/sk_lion.sql
+++ source/sk_lion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Agu 2019 pada 18.39
+-- Waktu pembuatan: 19 Agu 2019 pada 14.47
 -- Versi server: 10.1.40-MariaDB
 -- Versi PHP: 7.1.29
 
@@ -87,7 +87,81 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`no_aplikasi`, `gender`, `nama`, `alamat`, `kota`, `kode_pos`, `no_handphone`, `kebangsaan`, `no_identitas`, `email`, `nama_perusahaan`, `alamat_perusahaan`, `kota_perusahaan`, `kode_pos_perusahaan`, `jabatan`, `no_tlp`, `no_fax`, `email_perusahaan`, `bidang_usaha`, `alamat_surat`, `tgl_pengajuan`, `status`, `lampiran_daftar`) VALUES
-('00000000001', 'Mr', 'Haviz Indra Maulana', 'Jl. KH Moch Mansyur Rt. 010 Rw. 05', 'Jakarta Barat', '11210', '081355754092', 'WNA', '3339991111333666', 'viz.ndinq@gmail.com', 'PT. CodeManiac Indo', 'Jl. Dimana mana', 'Jakarta Barat', '11210', 'CEO', '081355754092', '021123123', 'codemanindo@gmail.com', 'Jasa Joki', 'Rumah', '2019-08-01 16:10:33', 'Terima', '00000000001.jpg');
+('00000000001', 'Mr', 'Haviz Indra Maulana', 'Jl. KH Moch Mansyur Rt. 010 Rw. 05', 'Jakarta Barat', '11210', '081355754092', 'WNA', '3339991111333666', 'viz.ndinq@gmail.com', 'PT. CodeManiac Indo', 'Jl. Dimana mana', 'Jakarta Barat', '11210', 'CEO', '081355754092', '021123123', 'codemanindo@gmail.com', 'Jasa Joki', 'Rumah', '2019-08-01 16:10:33', 'Terima', '00000000001.jpg'),
+('00000000002', 'Mr', 'Haviz Indra Maulana', 'Jakarta', 'DKI Jakarta', '11210', '081355754092', 'WNI', '3339991111333666', 'haviz_im@outlook.com', '', '', '', '', '', '', '', '', '', 'Rumah', '2019-08-17 18:31:40', 'Proses', '00000000002.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `flight`
+--
+
+CREATE TABLE `flight` (
+  `no_flight` varchar(6) NOT NULL,
+  `departure` varchar(3) NOT NULL,
+  `departure_date` date NOT NULL,
+  `departure_time` time NOT NULL,
+  `arrival` varchar(3) NOT NULL,
+  `arrival_date` date NOT NULL,
+  `arrival_time` time NOT NULL,
+  `duration` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `flight`
+--
+
+INSERT INTO `flight` (`no_flight`, `departure`, `departure_date`, `departure_time`, `arrival`, `arrival_date`, `arrival_time`, `duration`) VALUES
+('JT001', 'CGK', '2019-08-20', '13:00:00', 'SUB', '2019-08-20', '09:00:00', '04:00:00'),
+('JT002', 'CGK', '2019-08-20', '14:00:00', 'SUB', '2019-08-20', '10:00:00', '04:00:00'),
+('JT131', 'CGK', '2019-08-20', '17:25:00', 'KNO', '2019-08-20', '13:00:00', '04:25:00'),
+('JT204', 'KNO', '2019-08-20', '13:10:00', 'CGK', '2019-08-20', '10:50:00', '02:20:00'),
+('JT206', 'KNO', '2019-08-20', '18:30:00', 'CGK', '2019-08-20', '16:10:00', '02:20:00'),
+('JT294', 'KNO', '2019-08-20', '19:30:00', 'CGK', '2019-08-20', '16:00:00', '03:30:00'),
+('JT295', 'CGK', '2019-08-20', '20:15:00', 'KNO', '2019-08-20', '16:40:00', '03:35:00'),
+('JT303', 'CGK', '2019-08-20', '14:15:00', 'KNO', '2019-08-20', '11:50:00', '02:25:00'),
+('JT305', 'CGK', '2019-08-20', '20:40:00', 'KNO', '2019-08-20', '18:20:00', '02:25:00'),
+('JT306', 'KNO', '2019-08-20', '14:10:00', 'CGK', '2019-08-20', '11:50:00', '02:20:00'),
+('JT308', 'KNO', '2019-08-20', '07:05:00', 'CGK', '2019-08-20', '04:45:00', '02:20:00'),
+('JT309', 'CGK', '2019-08-20', '21:35:00', 'KNO', '2019-08-20', '19:10:00', '02:25:00'),
+('JT382', 'KNO', '2019-08-20', '15:10:00', 'CGK', '2019-08-20', '12:50:00', '02:20:00'),
+('JT383', 'CGK', '2019-08-20', '18:15:00', 'KNO', '2019-08-20', '15:50:00', '02:25:00'),
+('JT385', 'CGK', '2019-08-20', '20:10:00', 'KNO', '2019-08-20', '17:45:00', '02:25:00'),
+('JT387', 'CGK', '2019-08-20', '16:15:00', 'KNO', '2019-08-20', '13:50:00', '02:25:00'),
+('JT396', 'KNO', '2019-08-20', '19:45:00', 'CGK', '2019-08-20', '17:25:00', '02:20:00'),
+('JT397', 'CGK', '2019-08-20', '10:15:00', 'KNO', '2019-08-20', '07:50:00', '02:25:00'),
+('JT502', 'SRG', '2019-08-20', '16:35:00', 'CGK', '2019-08-20', '15:30:00', '01:05:00'),
+('JT503', 'CGK', '2019-08-20', '18:20:00', 'SRG', '2019-08-20', '17:15:00', '01:05:00'),
+('JT504', 'SRG', '2019-08-20', '09:05:00', 'CGK', '2019-08-20', '08:00:00', '01:05:00'),
+('JT505', 'CGK', '2019-08-20', '10:50:00', 'SRG', '2019-08-20', '09:45:00', '01:05:00'),
+('JT506', 'SRG', '2019-08-20', '12:25:00', 'CGK', '2019-08-20', '11:20:00', '01:05:00'),
+('JT507', 'CGK', '2019-08-20', '14:10:00', 'SRG', '2019-08-20', '13:05:00', '01:05:00'),
+('JT530', 'SOC', '2019-08-20', '20:45:00', 'CGK', '2019-08-20', '19:30:00', '01:15:00'),
+('JT536', 'SOC', '2019-08-20', '09:05:00', 'CGK', '2019-08-20', '07:50:00', '01:15:00'),
+('JT537', 'CGK', '2019-08-20', '11:00:00', 'SOC', '2019-08-20', '09:45:00', '01:10:00'),
+('JT538', 'SOC', '2019-08-20', '17:00:00', 'CGK', '2019-08-20', '15:45:00', '01:15:00'),
+('JT539', 'CGK', '2019-08-20', '18:55:00', 'SOC', '2019-08-20', '17:40:00', '01:10:00'),
+('JT561', 'CGK', '2019-08-20', '09:00:00', 'SOC', '2019-08-20', '07:50:00', '01:10:00'),
+('JT571', 'CGK', '2019-08-20', '08:15:00', 'SUB', '2019-08-20', '06:45:00', '01:30:00'),
+('JT573', 'CGK', '2019-08-20', '10:35:00', 'SUB', '2019-08-20', '09:05:00', '01:30:00'),
+('JT577', 'CGK', '2019-08-20', '15:20:00', 'SUB', '2019-08-20', '13:50:00', '01:30:00'),
+('JT590', 'SUB', '2019-08-20', '22:15:00', 'CGK', '2019-08-20', '20:45:00', '01:30:00'),
+('JT595', 'CGK', '2019-08-20', '23:00:00', 'SUB', '2019-08-20', '21:30:00', '01:30:00'),
+('JT596', 'SUB', '2019-08-20', '17:40:00', 'CGK', '2019-08-20', '16:10:00', '01:30:00'),
+('JT598', 'SUB', '2019-08-20', '15:50:00', 'CGK', '2019-08-20', '14:20:00', '01:30:00'),
+('JT599', 'CGK', '2019-08-20', '18:00:00', 'SUB', '2019-08-20', '16:30:00', '01:30:00'),
+('JT690', 'SUB', '2019-08-20', '06:30:00', 'CGK', '2019-08-20', '05:00:00', '01:30:00'),
+('JT692', 'SUB', '2019-08-20', '10:40:00', 'CGK', '2019-08-20', '09:10:00', '01:30:00'),
+('JT693', 'CGK', '2019-08-20', '18:20:00', 'SUB', '2019-08-20', '16:50:00', '01:30:00'),
+('JT694', 'SUB', '2019-08-20', '12:50:00', 'CGK', '2019-08-20', '11:20:00', '01:30:00'),
+('JT695', 'CGK', '2019-08-20', '15:20:00', 'SUB', '2019-08-20', '13:50:00', '01:30:00'),
+('JT696', 'SUB', '2019-08-20', '18:20:00', 'CGK', '2019-08-20', '16:50:00', '01:30:00'),
+('JT697', 'CGK', '2019-08-20', '20:45:00', 'SUB', '2019-08-20', '19:15:00', '01:30:00'),
+('JT748', 'SUB', '2019-08-20', '06:30:00', 'CGK', '2019-08-20', '05:00:00', '01:30:00'),
+('JT749', 'CGK', '2019-08-20', '16:05:00', 'SUB', '2019-08-20', '14:35:00', '01:30:00'),
+('JT821', 'CGK', '2019-08-20', '22:30:00', 'SUB', '2019-08-20', '21:00:00', '01:30:00'),
+('JT889', 'CGK', '2019-08-20', '21:40:00', 'SUB', '2019-08-20', '20:10:00', '01:30:00'),
+('JT890', 'SUB', '2019-08-20', '06:00:00', 'CGK', '2019-08-20', '04:30:00', '01:30:00');
 
 -- --------------------------------------------------------
 
@@ -158,7 +232,26 @@ INSERT INTO `log` (`id_log`, `user`, `id_ref`, `refrensi`, `keterangan`, `katego
 (264, '12345', '-', 'Auth', 'User logout', 'Logout', '2019-08-01 16:33:46'),
 (265, 'helpdesk', '-', 'Auth', 'User login', 'Login', '2019-08-01 16:33:57'),
 (266, 'helpdesk', '-', 'Auth', 'User logout', 'Logout', '2019-08-01 16:34:18'),
-(267, '101092', '-', 'Auth', 'User login', 'Login', '2019-08-01 16:34:26');
+(267, '101092', '-', 'Auth', 'User login', 'Login', '2019-08-01 16:34:26'),
+(268, '101092', '-', 'Auth', 'User logout', 'Logout', '2019-08-01 16:46:37'),
+(269, 'helpdesk', '-', 'Auth', 'User login', 'Login', '2019-08-05 14:17:56'),
+(270, 'helpdesk', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 14:19:20'),
+(271, '141939', '-', 'Auth', 'User login', 'Login', '2019-08-05 14:19:40'),
+(272, '141939', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 15:00:58'),
+(273, 'helpdesk', '-', 'Auth', 'User login', 'Login', '2019-08-05 15:01:05'),
+(274, 'helpdesk', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 15:01:26'),
+(275, '12345', '-', 'Auth', 'User login', 'Login', '2019-08-05 15:01:30'),
+(276, '12345', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 15:06:37'),
+(277, '12345', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 15:06:46'),
+(278, '141939', '-', 'Auth', 'User login', 'Login', '2019-08-05 15:44:04'),
+(279, '141939', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 15:49:51'),
+(280, 'helpdesk', '-', 'Auth', 'User login', 'Login', '2019-08-05 15:50:10'),
+(281, 'helpdesk', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 15:51:53'),
+(282, '12345', '-', 'Auth', 'User login', 'Login', '2019-08-05 15:51:58'),
+(283, '12345', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 15:56:29'),
+(284, 'helpdesk', '-', 'Auth', 'User login', 'Login', '2019-08-05 17:19:09'),
+(285, 'helpdesk', '-', 'Auth', 'User logout', 'Logout', '2019-08-05 17:19:20'),
+(286, '12345', '-', 'Auth', 'User login', 'Login', '2019-08-05 17:19:24');
 
 -- --------------------------------------------------------
 
@@ -174,13 +267,6 @@ CREATE TABLE `log_poin` (
   `type` enum('claim','redeem','','') NOT NULL,
   `tgl_log` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `log_poin`
---
-
-INSERT INTO `log_poin` (`id_user_poin`, `kode_booking`, `id_poin`, `no_member`, `type`, `tgl_log`) VALUES
-(20, 'BNAIDN', 'POIN0001', '30491728056', 'claim', '2019-08-01 16:22:05');
 
 -- --------------------------------------------------------
 
@@ -269,6 +355,13 @@ CREATE TABLE `redeem` (
   `keterangan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `redeem`
+--
+
+INSERT INTO `redeem` (`id_redeem`, `no_member`, `tgl_redeem`, `status_redeem`, `keterangan`) VALUES
+('RD-00000001', '30491728056', '2019-08-19 10:20:33', 'Proses', '');
+
 -- --------------------------------------------------------
 
 --
@@ -283,6 +376,13 @@ CREATE TABLE `redeem_detail` (
   `gender_pessenger` enum('Mr','Mrs','Ms','') NOT NULL,
   `nama_pessenger` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `redeem_detail`
+--
+
+INSERT INTO `redeem_detail` (`id_redeem_detail`, `id_redeem`, `id_poin`, `no_flight`, `gender_pessenger`, `nama_pessenger`) VALUES
+(1, 'RD-00000001', 'POIN0001', 'JT002', 'Mr', 'Haviz Indra Maulana');
 
 -- --------------------------------------------------------
 
@@ -335,6 +435,14 @@ ALTER TABLE `claim_detail`
 --
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`no_aplikasi`);
+
+--
+-- Indeks untuk tabel `flight`
+--
+ALTER TABLE `flight`
+  ADD PRIMARY KEY (`no_flight`),
+  ADD KEY `arrival` (`arrival`),
+  ADD KEY `departure` (`departure`);
 
 --
 -- Indeks untuk tabel `log`
@@ -401,25 +509,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `claim_detail`
 --
 ALTER TABLE `claim_detail`
-  MODIFY `id_claim_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_claim_detail` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `log`
 --
 ALTER TABLE `log`
-  MODIFY `id_log` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=268;
+  MODIFY `id_log` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=287;
 
 --
 -- AUTO_INCREMENT untuk tabel `log_poin`
 --
 ALTER TABLE `log_poin`
-  MODIFY `id_user_poin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_user_poin` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `redeem_detail`
 --
 ALTER TABLE `redeem_detail`
-  MODIFY `id_redeem_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_redeem_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -437,6 +545,13 @@ ALTER TABLE `claim`
 ALTER TABLE `claim_detail`
   ADD CONSTRAINT `claim_detail_ibfk_1` FOREIGN KEY (`id_claim`) REFERENCES `claim` (`id_claim`) ON UPDATE CASCADE,
   ADD CONSTRAINT `claim_detail_ibfk_2` FOREIGN KEY (`id_poin`) REFERENCES `master_poin` (`id_poin`);
+
+--
+-- Ketidakleluasaan untuk tabel `flight`
+--
+ALTER TABLE `flight`
+  ADD CONSTRAINT `flight_ibfk_1` FOREIGN KEY (`arrival`) REFERENCES `master_rute` (`id_rute`),
+  ADD CONSTRAINT `flight_ibfk_2` FOREIGN KEY (`departure`) REFERENCES `master_rute` (`id_rute`);
 
 --
 -- Ketidakleluasaan untuk tabel `log`
